d4a3fcd55b89d89761f2f2388ed234d152467ef4
GenerateMe <generateme.blog@gmail.com>
2020-12-20 01:18:47 +0100

    19fix

--- src/advent_of_code_2020/day19.clj
+++ src/advent_of_code_2020/day19.clj
@@ -6,13 +6,12 @@
 
 (defn build-tree
   [block]
-  (let [mem (into {} (map (fn [line]
-                            (let [[id r] (-> #"(\d+)\:\s(.*)"
-                                             (re-find (str/escape line {\" ""}))
-                                             (rest))
-                                  ors (str/split r #" \| ")]
-                              [id (map #(str/split % #" ")ors)])) block))]    
-    mem))
+  (into {} (map (fn [line]
+                  (let [[id r] (-> #"(\d+)\:\s(.*)"
+                                   (re-find (str/escape line {\" ""}))
+                                   (rest))
+                        ors (str/split r #" \| ")]
+                    [id (map #(str/split % #" ")ors)])) block)))
 
 (defn build-regex
   [tree id]
@@ -51,69 +50,3 @@
 
 (def part-2 (matches data regex2))
 ;; => 346
-
-(def data2 (common/str-as-blocks "42: 9 14 | 10 1
-9: 14 27 | 1 26
-10: 23 14 | 28 1
-1: a
-11: 42 31
-5: 1 14 | 15 1
-19: 14 1 | 14 14
-12: 24 14 | 19 1
-16: 15 1 | 14 14
-31: 14 17 | 1 13
-6: 14 14 | 1 14
-2: 1 24 | 14 4
-0: 8 11
-13: 14 3 | 1 12
-15: 1 | 14
-17: 14 2 | 1 7
-23: 25 1 | 22 14
-28: 16 1
-4: 1 1
-20: 14 14 | 1 15
-3: 5 14 | 16 1
-27: 1 6 | 14 18
-14: b
-21: 14 1 | 1 14
-25: 1 1 | 1 14
-22: 14 14
-8: 42
-26: 14 22 | 1 20
-18: 15 15
-7: 14 5 | 1 21
-24: 14 1
-
-abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
-bbabbbbaabaabba
-babbbbaabbbbbabbbbbbaabaaabaaa
-aaabbbbbbaaaabaababaabababbabaaabbababababaaa
-bbbbbbbaaaabbbbaaabbabaaa
-bbbababbbbaaaaaaaabbababaaababaabab
-ababaaaaaabaaab
-ababaaaaabbbaba
-baabbaaaabbaaaababbaababb
-abbbbabbbbaaaababbbbbbaaaababb
-aaaaabbaabaaaaababaa
-aaaabbaaaabbaaa
-aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
-babaaabbbaaabaababbaabababaaab
-aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"))
-
-
-
-(count (filter #(re-matches regex2 %) (second data)))
-
-
-(filter #(re-find regex8 %) (second data))
-
-(def part-2 (filter #(do
-                       (println %)
-                       (re-matches regex2 %)) (filter (complement part-1) (second data))))
-
-(count part-2)
-
-(let [b (build-tree (first data))]
-  (b "8"))
-
-(re-matches #"(:?[a-z])+" "zzzaz")
