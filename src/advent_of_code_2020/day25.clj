dd8e7512e5d2e271530ce1f20c3d82b75cdad43b
GenerateMe <generateme.blog@gmail.com>
2020-12-25 18:16:15 +0100

    opt

--- src/advent_of_code_2020/day25.clj
+++ src/advent_of_code_2020/day25.clj
@@ -1,21 +1,12 @@
 (ns advent-of-code-2020.day25)
 
-(def ^:const ^long d1 8184785)
-(def ^:const ^long d2 5293040)
-
-(def ^:const ^long m 20201227)
-
-(defn steps
-  [^long n]
-  (iterate (fn [^long i]
-             (mod (* i n) m)) n))
+(defn steps [^long n] (iterate (fn [^long i] (mod (* i n) 20201227)) n))
 
 (defn loop-number
   ^long [^long target]
   (->> (steps 7)
-       (map-indexed vector)
-       (drop-while (comp (fn [^long v] (not= v target)) second))
-       (ffirst)))
+       (keep-indexed (fn [id ^long v] (when (== v target) id)))
+       (first)))
 
-(def part-1 (nth (steps d1) (loop-number d2)))
+(def part-1 (nth (steps 8184785) (loop-number 5293040)))
 ;; => 4126980
