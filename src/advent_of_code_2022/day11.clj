73e1a82be22cd50e186c869f7032347984090efd
GenerateMe <generateme.blog@gmail.com>
2022-12-12 11:55:09 +0100

    some optimizations

--- src/advent_of_code_2022/day11.clj
+++ src/advent_of_code_2022/day11.clj
@@ -3,22 +3,19 @@
 
 (def get-numbers (partial re-seq #"\d+"))
 (def get-number (comp parse-long first get-numbers))
-
 (defn maybe-long [v] (when-not (= v "old") (parse-long v)))
 
 (defn parse
   [[monkey items op divisor t f]]
-  (let [[a o b] (drop 3 (split-line op))
-        fun (resolve (symbol o))
-        arga (maybe-long a)
-        argb (maybe-long b)]
-    {:id (get-number monkey)
-     :items (map parse-long (get-numbers items))
-     :divisor (get-number divisor)
-     :t (get-number t)
-     :f (get-number f)
-     :op (fn [arg] (fun (if arga arga arg) (if argb argb arg)))
-     :inspected 0}))
+  {:id (get-number monkey)
+   :items (map parse-long (get-numbers items))
+   :divisor (get-number divisor)
+   :t (get-number t)
+   :f (get-number f)
+   :op (let [[a o b] (drop 3 (split-line op))
+             fun (resolve (symbol o))
+             [arga argb] (map maybe-long [a b])]
+         (fn [arg] (fun (if arga arga arg) (if argb argb arg))))})
 
 (def data (mapv parse (read-data-as-blocks 2022 11)))
 
@@ -26,11 +23,11 @@
   [inspect-f monkeys id]
   (let [{:keys [items divisor t f op]} (monkeys id)]
     (-> (reduce (fn [m item]
-                  (let [wl (inspect-f op item)
+                  (let [wl (inspect-f (op item))
                         target (if (zero? (mod wl divisor)) t f)]
                     (update-in m [target :items] conj wl))) monkeys items)
         (update-in [id :items] empty)
-        (update-in [id :inspected] + (count items)))))
+        (update-in [id :inspected] (fnil + 0) (count items)))))
 
 (defn round
   [inspect-f monkeys]
@@ -45,14 +42,14 @@
        (reduce *)))
 
 (defn most-inspected1 [data]
-  (most-inspected data (fn [op item] (quot (op item) 3)) 20))
+  (most-inspected data (fn [item] (quot item 3)) 20))
 
 (def part-1 (most-inspected1 data))
 ;; => 110888
 
 (defn most-inspected2 [data]
   (most-inspected data (let [divisors (reduce * (map :divisor data))] ;; all are primes!
-                         (fn [op item] (mod (op item) divisors))) 10000))
+                         (fn [item] (mod item divisors))) 10000))
 
 (def part-2 (most-inspected2 data))
 ;; => 25590400731
