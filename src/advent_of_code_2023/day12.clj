b1ab5e37a5677d538eadd10e770d6d18aa71ce69
GenerateMe <generateme.blog@gmail.com>
2023-12-12 23:06:22 +0100

    some optimizations

--- src/advent_of_code_2023/day12.clj
+++ src/advent_of_code_2023/day12.clj
@@ -4,11 +4,11 @@
 
 (defn parse-line [l]
   (let [[rule struct] (str/split l #"\s+")]
-    [(vec (str "." rule)) (map parse-long (str/split struct #","))]))
+    [(vec (str "." rule)) (mapv parse-long (str/split struct #","))]))
 
 (def data (map parse-line (read-data 2023 12)))
 
-(defn match-substring [pattern start dots hashes]
+(defn match-substring [pattern start hashes dots]
   (let [mid (+ start dots)]
     (and (every? #(or (= % \.) (= % \?)) (subvec pattern start mid))
          (every? #(or (= % \#) (= % \?)) (subvec pattern mid (+ mid hashes))))))
@@ -17,28 +17,25 @@
   (memoize (fn ([[rule struct]] (find-combinations rule struct 0))
              ([rule struct pos]
               (if-not (seq struct)
-                (if (match-substring rule pos (- (count rule) pos) 0) 1 0)
-                (let [sum (reduce + (dec (count struct)) struct)
-                      mx (- (count rule) sum pos)
-                      hashes (first struct)]
-                  (->> (for [dots (range 1 (inc mx))
-                             :when (match-substring rule pos dots hashes)]
-                         (+ pos dots hashes))
-                       (map (partial find-combinations rule (rest struct)))
-                       (reduce +))))))))
+                (if (match-substring rule pos 0 (- (count rule) pos)) 1 0)
+                (let [hashes (struct 0)]
+                  (transduce (comp (filter (partial match-substring rule pos hashes))
+                                   (map (fn [d] (+ d pos hashes)))
+                                   (map (partial find-combinations rule (subvec struct 1))))
+                             + (range 1 (->> (reduce + (dec (count struct)) struct)
+                                             (- (count rule) pos -1))))))))))
 
 (defn counts
   ([data] (counts data identity))
-  ([data process]
-   (->> (map (comp find-combinations process) data)
-        (reduce +))))
+  ([data process] (->> (map (comp find-combinations process) data)
+                       (reduce +))))
 
 (def part-1 (counts data))
 ;; => 6958
 
 (defn repeat-rule [[r struct]]
   [(vec (conj (flatten (interpose \? (repeat 5 (subvec r 1)))) \.))
-   (flatten (repeat 5 struct))])
+   (vec (flatten (repeat 5 struct)))])
 
 (def part-2 (counts data repeat-rule))
 ;; => 6555315065024
