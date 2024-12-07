5754ae97aefb4187cad16d9ce11d4d79c2bfc477
GenerateMe <generateme.blog@gmail.com>
2024-12-07 16:58:43 +0100

    day07

---
+++ src/advent_of_code_2024/day07.clj
@@ -0,0 +1,42 @@
+(ns advent-of-code-2024.day07
+  (:require [common :refer [read-data get-numbers]]))
+
+(def data (map get-numbers (read-data 2024 07)))
+
+(defmacro ->gen-combinations [nm init]
+  `(def ~nm (memoize (fn [^long length#]
+                     (if (== 1 length#)
+                       [~@(map vector init)]
+                       (mapcat (fn [~'l] [~@(for [op init] `(conj ~'l ~op))]) (~nm (dec length#))))))))
+
+(->gen-combinations combinations2 [* +])
+
+(defn calculate
+  ^long [^long v r combination]
+  (->> (map vector r combination)
+       (reduce (fn [^long v1 [^long v2 op]] (op v1 v2)) v)))
+
+(defn check-line
+  [comb-fn [^long t ^long f & r]]
+  (if (->> (comb-fn (count r))
+           (some (fn [combination] (== t (calculate f r combination)))))
+    t 0))
+
+(defn answer [comb-fn data]
+  (->> data
+       (pmap (partial check-line comb-fn))
+       (reduce +)))
+
+(def part-1 (answer combinations2 data))
+;; => 850435817339
+
+(defn || ^long [^long a ^long b]
+  (cond
+    (< b 10) (+ (* a 10) b)
+    (< b 100) (+ (* a 100) b)
+    (< b 1000) (+ (* a 1000) b)))
+
+(->gen-combinations combinations3 [* + ||])
+
+(defonce part-2 (answer combinations3 data))
+;; => 104824810233437
