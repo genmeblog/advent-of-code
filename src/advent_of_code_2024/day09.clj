9ad53c8e7a85849ce19b9c3a437d38a927d62afd
GenerateMe <generateme.blog@gmail.com>
2024-12-10 01:12:10 +0100

    comments

--- src/advent_of_code_2024/day09.clj
+++ src/advent_of_code_2024/day09.clj
@@ -28,16 +28,19 @@
 (defn move-to-gap
   ([{:keys [files gaps total]}] (move-to-gap files gaps (dec total) 0))
   ([files [[gpos gsize] & gaps] fid buff]
-   (if (neg? fid)
+   (if (neg? fid) ;; the end of files
      buff
-     (let [[fpos fsize] (files fid)]
-       (if (<= fpos (or gpos ##Inf))
+     (let [[fpos fsize] (files fid)] ;; get current file posistion and size
+       (if (<= fpos (or gpos ##Inf)) ;; all gaps filled already, just calculate checksum for the rest
          (recur files gaps (dec fid) (+ buff (checksum fpos fsize fid)))
          (cond
-           (== gsize fsize) (recur files gaps (dec fid)
+           ;; gap and file sizes are the same
+           (== gsize fsize) (recur files gaps (dec fid) 
                                    (+ buff (checksum gpos fsize fid)))
+           ;; gap size bigger than file, fill it, make gap smaller, move to next file
            (> gsize fsize) (recur files (conj gaps [(+ gpos fsize) (- gsize fsize)]) (dec fid)
                                   (+ buff (checksum gpos fsize fid)))
+           ;; gaps size too small, fill it, move to next gap, update file size
            :else (recur (assoc files fid [fpos (- fsize gsize)]) gaps fid
                         (+ buff (checksum gpos gsize fid)))))))))
 
@@ -48,27 +51,29 @@
 (defn find-in-gaps
   ([gaps total fsize fpos] (find-in-gaps gaps total fsize fpos 0))
   ([gaps total fsize fpos id]
-   (when (< id total)
+   (when (< id total) ;; check if inside gap list
      (let [[gpos gsize] (gaps id)]
-       (when (< gpos fpos)
-         (if (>= gsize fsize)
+       (when (< gpos fpos) ;; gap is before file
+         (if (>= gsize fsize) ;; file fits in a gap, return gap position and size
            [id gpos gsize]
+           ;; move to next gap
            (recur gaps total fsize fpos (inc id))))))))
 
 ;; Part 2
 (defn fit-in-gap
   ([{:keys [files gaps total]}] (fit-in-gap files gaps total (dec total) 0))
   ([files gaps total fid buff]
-   (if (neg? fid)
+   (if (neg? fid) ;; the end of files
      buff
      (let [[fpos fsize] (files fid)
-           gapdata (find-in-gaps gaps total fsize fpos)]
-       (if gapdata
+           gapdata (find-in-gaps gaps total fsize fpos)] ;; find valid gap
+       (if gapdata ;; gap is found
          (let [[gapid gpos gsize] gapdata
+               ;; update gap size
                ngaps (assoc gaps gapid [(+ gpos fsize) (- gsize fsize)])] 
-           (recur files ngaps total (dec fid)
+           (recur files ngaps total (dec fid) ;; calculate checksum for a file in found gap
                   (+ buff (checksum gpos fsize fid))))
-         (recur files gaps total (dec fid)
+         (recur files gaps total (dec fid) ;; no gap, do not move file, calculate checksum
                 (+ buff (checksum fpos fsize fid))))))))
 
 (def part-2 (fit-in-gap data))
